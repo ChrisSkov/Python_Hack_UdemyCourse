3003456fa0d0a639e3f1d2b3d3056fb2791e528d
Chris <stofferdyr@gmail.com>
2022-01-30 10:02:09 -0500

    start lesson 14

--- PythonHacks/portscanner.py
+++ PythonHacks/portscanner.py
@@ -1,12 +1,20 @@
 import socket
 from IPy import IP
 
-ipaddress = input('[+] Enter scanning target: ')
+
 port = 80
 
-try:
-    sock = socket.socket()
-    sock.connect((ipaddress, port))
-    print('[+] Port 80 is open')
-except:
-    print('[-] Port 80 is closed')
+
+def scan_port(ipaddress, port):
+    try:
+        sock = socket.socket()
+        sock.settimeout(0.5)
+        sock.connect((ipaddress, port))
+        print('[+] Port' + str(port) + ' is open')
+    except:
+        print('[-] Port ' + str(port) + ' is closed')
+
+ipaddress = input('[+] Enter scanning target: ')
+
+for port in range(75, 85):
+    scan_port(ipaddress,port)
