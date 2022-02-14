044ac8bb1799b5e058a60f5ae95b2dd49765369c
Chris <stofferdyr@gmail.com>
2022-02-14 09:35:23 -0500

    start lesson 36

--- passwordsniffer/pass-sniffer.py
+++ passwordsniffer/pass-sniffer.py
@@ -5,9 +5,13 @@ import re
 iface = "eth0"
 
 def get_login_pass():
-
+    user = None
+    passwd = None
 
 def pkt_parser(packet):
+    if packet.haslayer(TCP) and packet.haslayer(Raw) and packet.haslayer(IP):
+        body = str(packet[TCP].payload)
+        get_login_pass(body)
 
 
 try:
