d6a46392ef79397c2dfd367ccb47429d9d084220
Chris <stofferdyr@gmail.com>
2022-02-25 07:32:16 -0500

    start lesson 63

--- email_attacks/email-scraper.py
+++ email_attacks/email-scraper.py
@@ -21,4 +21,17 @@ try:
             break
         url = urls.popleft()
         scraped_urls.add(url)
-        
+
+        parts = urllib.parse.urlsplit(url)
+        base_url = '{0.scheme}://{0.netloc}'.format(parts)
+
+        path = url[:url.rfind('/') +1] if '/' in parts.path else url
+
+        print('[%d] Processing %s' % (count, url))
+        try:
+            response = requests.get(url)
+        except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):
+            continue
+
+        new_emails = set(re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", response.text, re.I))
+        emails.update(new_emails)
