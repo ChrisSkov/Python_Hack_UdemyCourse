32fbcd7ff54cabea351ae3c42bfb305142d5fabb
Chris <stofferdyr@gmail.com>
2022-03-07 15:11:44 -0500

    clean up gui code. add tabs

--- Gui_test/main.py
+++ Gui_test/main.py
@@ -9,6 +9,7 @@ def start_nmap(args):
     command_to_run = 'nmap' + args
     es.update_output_text(window=my_ui.window, field_to_update='-OUTPUT-',
                           new_text='Running command: ' + command_to_run)
+    # sg.popup(command_to_run, keep_on_top=False)
     sp = subprocess.run(command_to_run, shell=True, text=True, capture_output=False)
 
     # sp = subprocess.Popen(['nmap'] + [arg_string], stdin=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -20,28 +21,42 @@ def start_nmap(args):
 class UITing:
     do_scrape = False
 
-    # key must be the same as metadata but with an added '-' at the end
-    column = [[sg.Checkbox('OS Detection', default=False, auto_size_text=True, key='-O-', metadata='-O'),
-               sg.Checkbox('OS and version detection', key='-A-', metadata='-A'),
-               sg.Checkbox('T4', key='-T4-', metadata='-T4'),
-               sg.Checkbox('Treat all hosts as online', key='-Pn-', metadata='-Pn')],
-              [sg.Checkbox('Fast mode', key='-F-', metadata='-F')]]
+    # key must be the same as metadata
+    column = [[sg.Checkbox('OS Detection', default=False, auto_size_text=True, key='-O', metadata='-O'),
+               sg.Checkbox('OS and version detection', key='-A', metadata='-A'),
+               sg.Checkbox('T4', key='-T4', metadata='-T4'),
+               sg.Checkbox('Treat all hosts as online', key='-Pn', metadata='-Pn')],
+              [sg.Checkbox('Fast mode', key='-F', metadata='-F')]]
 
     mini_column = [[sg.Text(size=(150, 290), key='-OUTPUT-', auto_size_text=True, expand_y=True, expand_x=True)]]
-
     full_column = sg.Column(column)
+
     little_column = sg.Column(mini_column, scrollable=True, vertical_scroll_only=True, expand_x=True)
 
-    layout = [[sg.Text('Enter Scan target')],
-              [sg.Input(key='-INPUT-', pad=(12, 0), expand_x=True),
-               sg.Button('Scan!', bind_return_key=True),
-               sg.Button('Scrape emails'),
-               sg.Button('Never-mind')], [full_column], [little_column]]
+    cross_tab_layout = [[[sg.Text('Enter target', font='15')], [sg.Input(key='-INPUT-', pad=(8, 0), expand_x=True)]]]
+
+    scan_options = [[
+        sg.Button('Scan!', bind_return_key=True),
+        sg.Button('Scrape emails'),
+        sg.Button('Never-mind')], [full_column], [little_column]]
+    layout = cross_tab_layout
+    layout += [[sg.TabGroup([[sg.Tab('Scan tings', scan_options)]])]]
+
 
     layout[-1].append(sg.Sizegrip())
     window = sg.Window('Script Kiddie Toolbox', layout, location=(1050, 300), resizable=True, size=(1000, 550))
 
     # process-factory.dk
+
+
+def get_scan_args():
+    arg_string = ''
+    for i in range(0, len(my_ui.column)):
+        for checkbox in my_ui.column[i]:
+            # checkbox_val = str(checkbox.metadata + '-')
+            if values[checkbox.metadata]:
+                arg_string += ' ' + checkbox.metadata
+    return arg_string
 
 
 if __name__ == '__main__':
@@ -51,15 +66,9 @@ if __name__ == '__main__':
         if event == sg.WINDOW_CLOSED or event == 'Never-mind':
             break
         elif event == 'Scan!' or event == 'Return':
-            es.update_output_text(window=my_ui.window, field_to_update='-OUTPUT-', new_text='yo')
-            arg_string = ''
-            for i in range(0, len(my_ui.column)):
-                for checkbox in my_ui.column[i]:
-                    checkbox_val = str(checkbox.metadata + '-')
-                    if values[checkbox_val]:
-                        arg_string += ' ' + checkbox.metadata
-                        arg_string += ' ' + values['-INPUT-']
-            start_nmap(arg_string)
+            args = get_scan_args()
+            args += ' ' + values['-INPUT-']
+            start_nmap(args)
         elif event == 'Scrape emails':
             scrape_me = es.EmailScraper()
             es.EmailScraper.scrape_emails(self=scrape_me, target=values['-INPUT-'], window=my_ui.window)
