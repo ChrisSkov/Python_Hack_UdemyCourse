0c436323739d4f785aa7aeb855caf506960d155f
Chris <stofferdyr@gmail.com>
2022-03-15 05:43:30 -0400

    slightly faster. better output

--- LogParser/parse_my_logs.py
+++ LogParser/parse_my_logs.py
@@ -4,12 +4,14 @@ import datetime
 # res = DbIpCity.get('45.146.165.37', api_key='free')
 # full_country_name = pycountry.countries.get(alpha_2=res.country)
 # print(res.city + ' ' + full_country_name.name)
-path_to_log = '/home/kali/Desktop/log1.txt'
+path_to_log = '/home/kali/Desktop/log.txt'
 
 black_list = open('./banned_requests.txt')
 sus = black_list.readlines()
 sus = sus[0].split(',')
 my_file = open(path_to_log).readlines()
+
+my_dict = {}
 
 
 # TODO: optimize for speed
@@ -23,29 +25,22 @@ def cmp_file_to_wordlist():
     results_name = 'log_results_%s.txt' % (datetime.datetime.now().date())
     results = open(results_name, 'a')
     for line in my_file:
-        line_to_print = find_info_from_ip(line)
         for item in sus[0:len(sus)]:
             if item in line:
-                print('INTRUSION ATTEMPT ' + line_to_print)
+                line_to_print = find_info_from_ip(line)
+                print('INTRUSION ATTEMPT ' + line_to_print[0])
                 intrusion_attempts += 1
-                results.write('\nINTRUSION ATTEMPT ' + line_to_print)
+                results.write('\nINTRUSION ATTEMPT ' + line_to_print[0])
+                country = pycountry.countries.get(alpha_2=line_to_print[1].country)
+                if country.name not in my_dict:
+                    my_dict[country.name] = 1
+                else:
+                    my_dict[country.name] = my_dict[country.name] + 1
                 break
         lines += 1
     print(intrusion_attempts, lines)
 
 
-def loop_lists():
-    intrusion_attempts = 0
-    lines = 0
-    for item in sus[0:len(sus)]:
-        for line in my_file:
-            line_to_print = find_info_from_ip(line)
-            if item in line:
-                print('INTRUSION ATTEMPT ' + line_to_print)
-                intrusion_attempts += 1
-                break
-        lines += 1
-        print(intrusion_attempts, lines)
 
 
 def find_info_from_ip(line):
@@ -54,9 +49,11 @@ def find_info_from_ip(line):
     request_info = DbIpCity.get(str(request_ip), api_key='free')
     req_country = pycountry.countries.get(alpha_2=request_info.country)
     line_to_print = request_ip + ' ' + request_info.city + ' ' + req_country.name + ' ' + req_country.flag
-    return line_to_print
+    result = (line_to_print, request_info)
+    return result
 
 
 if __name__ == '__main__':
     # loop_lists()
     cmp_file_to_wordlist()
+    print(my_dict)
