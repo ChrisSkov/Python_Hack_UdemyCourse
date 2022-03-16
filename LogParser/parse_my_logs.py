8b581f13009a32812befa4b548f24d187a29eb19
Chris <stofferdyr@gmail.com>
2022-03-16 11:51:13 -0400

    refactor code

--- LogParser/parse_my_logs.py
+++ LogParser/parse_my_logs.py
@@ -1,10 +1,11 @@
 from ip2geotools.databases.noncommercial import DbIpCity
 import pycountry
 import datetime
+
 # res = DbIpCity.get('45.146.165.37', api_key='free')
 # full_country_name = pycountry.countries.get(alpha_2=res.country)
 # print(res.city + ' ' + full_country_name.name)
-path_to_log = '/home/kali/Desktop/log.txt'
+path_to_log = '/home/kali/Desktop/log1.txt'
 
 black_list = open('./banned_requests.txt')
 sus = black_list.readlines()
@@ -31,29 +32,25 @@ def cmp_file_to_wordlist():
                 print('INTRUSION ATTEMPT ' + line_to_print[0])
                 intrusion_attempts += 1
                 results.write('\nINTRUSION ATTEMPT ' + line_to_print[0])
-                country = pycountry.countries.get(alpha_2=line_to_print[1].country)
-                if country.name not in my_dict:
-                    my_dict[country.name] = 1
+                country = line_to_print[1].name
+                if country not in my_dict:
+                    my_dict[country] = 1
                 else:
-                    my_dict[country.name] = my_dict[country.name] + 1
+                    my_dict[country] = my_dict[country] + 1
                 break
         lines += 1
     print(intrusion_attempts, lines)
 
 
-
-
 def find_info_from_ip(line):
-    new_string = line.find(' ')
-    request_ip = line[0:new_string]
+    request_ip = line[0:line.find(' ')]
     request_info = DbIpCity.get(str(request_ip), api_key='free')
     req_country = pycountry.countries.get(alpha_2=request_info.country)
     line_to_print = request_ip + ' ' + request_info.city + ' ' + req_country.name + ' ' + req_country.flag
-    result = (line_to_print, request_info)
+    result = (line_to_print, req_country)
     return result
 
 
 if __name__ == '__main__':
-    # loop_lists()
     cmp_file_to_wordlist()
     print(my_dict)
