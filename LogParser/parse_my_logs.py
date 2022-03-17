cb199c5b150dd1755dd629821d2783283319ac44
Chris <stofferdyr@gmail.com>
2022-03-17 09:05:45 -0400

    add whitelist function

--- LogParser/parse_my_logs.py
+++ LogParser/parse_my_logs.py
@@ -1,16 +1,19 @@
 from ip2geotools.databases.noncommercial import DbIpCity
 import pycountry
 import datetime
-
+import re
 # res = DbIpCity.get('45.146.165.37', api_key='free')
 # full_country_name = pycountry.countries.get(alpha_2=res.country)
 # print(res.city + ' ' + full_country_name.name)
-path_to_log = '/home/kali/Desktop/log1.txt'
-
+path_to_log = open('/home/kali/Desktop/log.txt', 'r')
 black_list = open('./banned_requests.txt')
-sus = black_list.readlines()
-sus = sus[0].split(',')
-my_file = open(path_to_log).readlines()
+white_list = open('./white_list.txt')
+
+log_to_read = path_to_log.readlines()
+block_list = black_list.readlines()
+block_list = block_list[0].split(',')
+allow_list = white_list.readlines()
+allow_list = allow_list[0].split(',')
 
 my_dict = {}
 
@@ -18,19 +21,18 @@ my_dict = {}
 # TODO: optimize for speed
 # white list > black list
 # how to output?
-
-
-def cmp_file_to_wordlist():
+def cmp_file_to_blacklist():
     intrusion_attempts = 0
     lines = 0
     results_name = 'log_results_%s.txt' % (datetime.datetime.now().date())
     results = open(results_name, 'a')
-    for line in my_file:
-        for item in sus[0:len(sus)]:
+    for line in log_to_read:
+        line = line.lower()
+        for item in block_list[0:len(block_list)]:
             if item in line:
+                intrusion_attempts += 1
                 line_to_print = find_info_from_ip(line)
                 print('INTRUSION ATTEMPT ' + line_to_print[0])
-                intrusion_attempts += 1
                 results.write('\nINTRUSION ATTEMPT ' + line_to_print[0])
                 country = line_to_print[1].name
                 if country not in my_dict:
@@ -39,7 +41,34 @@ def cmp_file_to_wordlist():
                     my_dict[country] = my_dict[country] + 1
                 break
         lines += 1
-    print(intrusion_attempts, lines)
+        print(intrusion_attempts, lines)
+    close_files()
+
+
+def cmp_file_to_whitelist():
+    intrusion_attempts = 0
+    results_name = 'log_results_%s.txt' % (datetime.datetime.now().date())
+    results = open(results_name, 'a')
+    for line in log_to_read:
+        line_to_regex = line[line.index('"'):]
+        line_to_regex = line_to_regex[1:line_to_regex[1:].find('"')+1]
+        regex = r"^(?:GET\s|POST\s)+(\/[a-zA-Z]+\/[a-zA-Z]+\s)(?:HTTP\/1.1)$"
+        matches = re.findall(regex, line_to_regex, re.MULTILINE)
+        if len(matches) == 0:
+            intrusion_attempts += 1
+            line_to_write = find_info_from_ip(line)
+            results.write(line_to_write[0] + '\n')
+        # for matchNum, match in enumerate(matches, start=1): print("Match {matchNum} was found at {start}-{end}: {
+        # match}".format(matchNum=matchNum, start=match.start(), end=match.end(), match=match.group()))
+
+    print(intrusion_attempts)
+
+
+
+def close_files():
+    path_to_log.close()
+    black_list.close()
+    white_list.close()
 
 
 def find_info_from_ip(line):
@@ -52,5 +81,6 @@ def find_info_from_ip(line):
 
 
 if __name__ == '__main__':
-    cmp_file_to_wordlist()
-    print(my_dict)
+    #cmp_file_to_blacklist()
+    cmp_file_to_whitelist()
+    #print(my_dict)
