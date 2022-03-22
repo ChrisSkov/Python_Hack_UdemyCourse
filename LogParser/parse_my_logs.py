c169bf6f772226cbbd1e0a8e08851857e521b6db
Chris <stofferdyr@gmail.com>
2022-03-22 04:59:00 -0400

    Almost done

--- LogParser/parse_my_logs.py
+++ LogParser/parse_my_logs.py
@@ -3,33 +3,22 @@ from ip2geotools.databases.noncommercial import DbIpCity
 import pycountry
 import datetime
 import re
-import os
 import yaml
 from yaml.loader import SafeLoader
 import glob
 
-# res = DbIpCity.get('45.146.165.37', api_key='free')
-# full_country_name = pycountry.countries.get(alpha_2=res.country)
-# print(res.city + ' ' + full_country_name.name)
-# path_to_log = open('/home/kali/Desktop/log.txt', 'r')
-black_list = './banned_requests.txt'
+# Path to black list
+black_list = './black_list.txt'
+# Path to while list
 white_list = './white_list.txt'
-dir_path = './'
 
+my_dict = {}
+
+# List for files to read
 files = []
 
 
-def regex_test():
-    with open('configme.yaml') as config:
-        data = yaml.load(config, Loader=SafeLoader)
-        regex1 = r"%s" % (data['parsing']['-name_pattern'])
-    for file in os.listdir('./'):
-        matches = re.findall(regex1, file, re.MULTILINE)
-        files.append(matches[0])
-        for match in matches:
-            files.append(match.group())
-
-
+# Word manipulation to put black and white list into list objects
 def wrangle_file(file_to_wrangle):
     file_to_wrangle = open(str(file_to_wrangle), 'r').readlines()
     wrangled_file = file_to_wrangle[0].split(',')
@@ -39,25 +28,30 @@ def wrangle_file(file_to_wrangle):
 block_list = wrangle_file(black_list)
 allow_list = wrangle_file(white_list)
 
-my_dict = {}
+
+# Find files in a directory according to pattern in configme.yaml
+def find_files_in_dir():
+    with open('configme.yaml') as config:
+        data = yaml.load(config, Loader=SafeLoader)
+        glob_pattern = data['parsing']['-name_pattern']
+
+    for file in glob.glob(glob_pattern):
+        files.append(file)
 
 
-# TODO: optimize for speed
-# white list > black list
-# how to output?
+# compare contents of file(s) to black list
 def cmp_file_to_blacklist():
-    regex_test()
+    find_files_in_dir()
     lines = 0
     intrusion_attempts, results = setup_result_file()
     for file in files:
         with open(file, 'r') as log_to_read:
-            for line in log_to_read:
+            for line in log_to_read.readlines():
                 line = line.lower()
                 for item in block_list[0:len(block_list)]:
                     if item in line:
                         intrusion_attempts += 1
                         line_to_print = find_info_from_ip(line)
-                        print('INTRUSION ATTEMPT ' + line_to_print[0])
                         results.write('\nINTRUSION ATTEMPT ' + line_to_print[0])
                         country = line_to_print[1].name
                         if country not in my_dict:
@@ -66,13 +60,12 @@ def cmp_file_to_blacklist():
                             my_dict[country] = my_dict[country] + 1
                         break
                 lines += 1
-                print(intrusion_attempts, lines)
-    close_files()
 
 
+# compare contents of file(s) to white list
 def cmp_file_to_whitelist():
     intrusion_attempts, results = setup_result_file()
-    regex_test()
+    find_files_in_dir()
     for file in files:
         with open(file, 'r') as log_to_read:
             for line in log_to_read:
@@ -86,6 +79,7 @@ def cmp_file_to_whitelist():
                     results.write(line_to_write[0] + '\n')
 
 
+# Sort out naming of result file
 def setup_result_file():
     intrusion_attempts = 0
     results_name = 'log_results_%s.txt' % (datetime.datetime.now().date())
@@ -93,21 +87,31 @@ def setup_result_file():
     return intrusion_attempts, results
 
 
-def close_files():
-    black_list.close()
-    white_list.close()
-
-
+# look up suspicious IP address
 def find_info_from_ip(line):
-    request_ip = line[0:line.find(' ')]
-    request_info = DbIpCity.get(request_ip, api_key='free')
-    req_country = pycountry.countries.get(alpha_2=request_info.country)
-    line_to_print = request_ip + ' ' + request_info.city + ' ' + req_country.name + ' ' + req_country.flag
-    result = (line_to_print, req_country)
-    return result
+    if line is not None:
+        request_ip = line[0:line.find(' ')]
+        request_info = DbIpCity.get(request_ip, api_key='free')
+        req_country = pycountry.countries.get(alpha_2=request_info.country)
+        line_to_print = request_ip + ' ' + request_info.city + ' ' + req_country.name + ' ' + req_country.flag
+        result = (line_to_print, req_country)
+        return result
+
+# Starts program according to parameters in the config file
+def start_program():
+    with open('configme.yaml') as config:
+        data = yaml.load(config, Loader=SafeLoader)
+        if data['parsing']['-black_list'] == 'True':
+            cmp_file_to_blacklist()
+        elif data['parsing']['-white_list'] == 'True':
+            cmp_file_to_whitelist()
+        elif data['parsing']['-black_list'] == 'True' and data['parsing']['-white_list'] == 'True':
+            cmp_file_to_blacklist()
+            cmp_file_to_whitelist()
 
 
 if __name__ == '__main__':
-    #cmp_file_to_whitelist()
-    cmp_file_to_blacklist()
-
+    try:
+        start_program()
+    except Exception:
+        print(Exception)
